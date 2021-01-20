a6c2d30c9204d2dcf47e09463f5c1111efdbddf5
chenc19940823 <76268688+chenc19940823@users.noreply.github.com>
2021-01-19 23:14:02 -0800

    Update charizard.js

--- charizard.js
+++ charizard.js
@@ -3,7 +3,7 @@ class Charizard {
 	constructor(game) {
         Object.assign(this, { game });
         
-        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/charizardX.png");
+        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/charizardX.PNG");
 
         this.animation = new Animator(this.spritesheet, 260, 77, 56, 48, 3, 0.3, 8, false, true)
         
